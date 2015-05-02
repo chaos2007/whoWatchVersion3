5887abd097ed3465a775c8affaf867b1a06b4be6
chaos2007 <chaos2007@gmail.com>
2015-05-02 09:03:10 -0500

    Commit changes for Basalt

--- src/main.c
+++ src/main.c
@@ -7,7 +7,18 @@ static GFont *s_date_font;
 static TextLayer *s_date_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
-static InverterLayer *inverter_layer;
+//static InverterLayer *inverter_layer;
+
+#ifdef PBL_COLOR
+    #define TIME_COLOR GColorBlue
+    #define DATE_COLOR GColorBlue
+    #define BACKGROUND GColorVividCerulean
+#else
+    #define TIME_COLOR GColorWhite
+    #define DATE_COLOR GColorWhite
+    #define BACKGROUND GColorBlack
+#endif
+
 
 static void update_time() {
   // Get a tm structure
@@ -40,23 +51,18 @@ static void main_window_load(Window *window) {
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DOCTOR_WHO_LOGO);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 140, 82));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  bitmap_layer_set_background_color(s_background_layer, GColorBlack);
-  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpAssignInverted);
+  bitmap_layer_set_background_color(s_background_layer, BACKGROUND);
     
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 54, 144, 56));
-  #ifdef PBL_COLOR
-  text_layer_set_background_color(s_time_layer, GColorBlue);
-  #else
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  #endif
-  text_layer_set_text_color(s_time_layer, GColorWhite);
+  s_time_layer = text_layer_create(GRect(0, 82, 144, 56));
+  text_layer_set_background_color(s_time_layer, BACKGROUND);
+  text_layer_set_text_color(s_time_layer, TIME_COLOR);
   text_layer_set_text(s_time_layer, "00:00");
    
   // Create date TextLayer  
-  s_date_layer = text_layer_create(GRect(0, 110, 144, 30));
-  text_layer_set_background_color(s_date_layer, GColorClear);
-  text_layer_set_text_color(s_date_layer, GColorWhite);
+  s_date_layer = text_layer_create(GRect(0, 138, 144, 30));
+  text_layer_set_background_color(s_date_layer, BACKGROUND);
+  text_layer_set_text_color(s_date_layer, DATE_COLOR);
   text_layer_set_text(s_date_layer, "Thu Aug 23");  
 
   // Improve the layout to be more like a watchface
@@ -70,15 +76,10 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
     
-  // Inverter Layer
-  inverter_layer = inverter_layer_create(GRect(0,0,144,168));
-  layer_add_child(window_get_root_layer(window), inverter_layer_get_layer(inverter_layer));
-  
-
   // Add it as a child layer to the Window's root layer
-  layer_add_child(inverter_layer_get_layer(inverter_layer), bitmap_layer_get_layer(s_background_layer));
-  layer_add_child(inverter_layer_get_layer(inverter_layer), text_layer_get_layer(s_time_layer));
-  layer_add_child(inverter_layer_get_layer(inverter_layer), text_layer_get_layer(s_date_layer));
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 
   //layer_set_hidden(inverter_layer_get_layer(inverter_layer), false);
   // Make sure the time is displayed from the start
@@ -101,7 +102,7 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
     
   // Destroy Inverter Layer.
-  inverter_layer_destroy(inverter_layer);
+  //inverter_layer_destroy(inverter_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
